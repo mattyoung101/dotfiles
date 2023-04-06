e7228a1fd6e1083b6bbda722c5bcb68a998a12e6
ethanol <33417736+ethanol20924@users.noreply.github.com>
2023-04-06 22:52:13 +1000

    added docker container plugin, no idea if it works

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -311,7 +311,7 @@ vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { de
 -- See `:help nvim-treesitter`
 require('nvim-treesitter.configs').setup {
   -- Add languages to be installed here that you want installed for treesitter
-  ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'typescript', 'help', 'vim', 'markdown', 'latex' },
+  ensure_installed = { 'c', 'cpp', 'lua', 'python', 'rust', 'help', 'vim', 'markdown', 'latex', 'jsonc' },
 
   -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
   auto_install = false,
