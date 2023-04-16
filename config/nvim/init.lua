2b187f763840b6c4275de87dd1b3747f932cdd35
ethanol20924 <33417736+ethanol20924@users.noreply.github.com>
2023-04-16 23:09:20 +1000

    add dashboard and enable icons on status bar

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -96,7 +96,7 @@ require('lazy').setup({
     -- See `:help lualine.txt`
     opts = {
       options = {
-        icons_enabled = false,
+        icons_enabled = true,
         theme = 'auto',
         component_separators = '|',
         section_separators = '',
