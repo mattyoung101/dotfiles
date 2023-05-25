91f072e7d91c570d99d5808d5f0132316013a07c
Matt Young <matt.young.1@outlook.com>
2023-05-25 17:41:42 +1000

    Fix help thing

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -263,7 +263,7 @@ vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { de
 -- See `:help nvim-treesitter`
 require('nvim-treesitter.configs').setup {
   -- Add languages to be installed here that you want installed for treesitter
-  ensure_installed = { 'c', 'cpp', 'lua', 'python', 'rust', 'help', 'vim', 'markdown', 'latex', 'jsonc' },
+  ensure_installed = { 'c', 'cpp', 'lua', 'python', 'rust', 'vimdoc', 'vim', 'markdown', 'latex', 'jsonc' },
 
   -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
   auto_install = false,
