0c57036aff66f60df5fddfc3deb8582b499cfcaf
Matt Young <matt.young.1@outlook.com>
2023-06-26 16:52:39 +1000

    Fix todo plugin highlighting too much stuff

--- config/nvim/lua/custom/plugins/todo.lua
+++ config/nvim/lua/custom/plugins/todo.lua
@@ -9,23 +9,22 @@ return {
         sign_priority = 8, -- sign priority
         -- keywords recognized as todo comments
         keywords = {
-          FIX = {
+          FIXME = {
             icon = " ", -- icon used for the sign, and in search results
             color = "error", -- can be a hex color, or a named color (see below)
-            alt = { "FIXME", "BUG", "FIXIT", "ISSUE" }, -- a set of other keywords that all map to this FIX keywords
             -- signs = false, -- configure signs for some keywords individually
           },
           TODO = { icon = " ", color = "warning" },
           HACK = { icon = " ", color = "warning" },
           WARN = { icon = " ", color = "warning", alt = { "WARNING", "XXX" } },
           PERF = { icon = "󱫌 ", color = "warning", alt = { "PERFORMANCE" } },
-          NOTE = { icon = " ", color = "hint", alt = { "INFO" } },
+          NOTE = { icon = " ", color = "hint" },
         },
         gui_style = {
           fg = "NONE", -- The gui style to use for the fg highlight group.
           bg = "BOLD", -- The gui style to use for the bg highlight group.
         },
-        merge_keywords = true, -- when true, custom keywords will be merged with the defaults
+        merge_keywords = false, -- when true, custom keywords will be merged with the defaults
         -- highlighting of the line containing the todo comment
         -- * before: highlights before the keyword (typically comment characters)
         -- * keyword: highlights of the keyword
