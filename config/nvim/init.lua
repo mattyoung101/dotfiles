80700da2b3ffcb88f89e7a3bfccb5616c6a8fee5
Matt Young <matt.young.1@outlook.com>
2023-11-20 17:40:59 +1000

    Update plugins, spelling, fixed indent-blankline, remove mdpreview

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -99,15 +99,7 @@ require('lazy').setup({
     },
   },
 
-  { -- Add indentation guides even on blank lines
-    'lukas-reineke/indent-blankline.nvim',
-    -- Enable `lukas-reineke/indent-blankline.nvim`
-    -- See `:help indent_blankline.txt`
-    opts = {
-      char = '┊',
-      show_trailing_blankline_indent = false,
-    },
-  },
+  { "lukas-reineke/indent-blankline.nvim", main = "ibl", opts = {} },
 
   -- "gc" to comment visual regions/lines
   { 'numToStr/Comment.nvim', opts = {} },
@@ -462,7 +454,7 @@ local servers = {
   texlab = {},
   rust_analyzer = {},
   neocmake = {},
-  kotlin_language_server = {},
+  --kotlin_language_server = {},
 
   --pyright = {},
   --jedi_language_server = {},
