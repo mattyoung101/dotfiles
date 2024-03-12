07ce58542ce2c4b669b9796ece0700811442b123
Matt Young <matt.young.1@outlook.com>
2024-03-12 17:40:47 +1000

    Add lazygit keybinding <leader>lg

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -291,6 +291,7 @@ vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc
 vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = '[S]earch current [W]ord' })
 vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
 vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
+vim.keymap.set('n', '<leader>lg', require('lazygit').lazygit, { desc = '[L]azy[G]it'})
 
 -- Barbar keybindings
 local bbmap = vim.api.nvim_set_keymap
