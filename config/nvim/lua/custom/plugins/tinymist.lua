mattyoung101/dotfiles

commit 388de30bbf2c93f34e7a3db5ba9dfc16cd0c5470

Attempt to disable preview in Tinymist

config/nvim/lua/custom/plugins/tinymist.lua
@@ -1,0 +1,17 @@
+return {
+  -- add tinymist to lspconfig
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        tinymist = {
+          settings = {
+              systemFonts = false,
+              formatterMode = "typstyle",
+              previewFeature = "disable"
+          }
+        },
+      },
+    },
+  },
+}

config/nvim/init.lua
@@ -509,7 +509,13 @@ local servers = {
   glsl_analyzer = {},
   yamlls = {},
   --typst_lsp = {},
-  tinymist = {},
+  tinymist = {
+    settings = {
+      systemFonts = false,
+      formatterMode = "typstyle",
+      previewFeature = "disable"
+    }
+  },
   robotframework_ls = {},
 
   -- for HTML
@@ -695,6 +701,3 @@ vim.g.rainbow_delimiters = {
         'RainbowDelimiterCyan',
     },
 }
-
--- The line beneath this is called `modeline`. See `:help modeline`
--- vim: ts=2 sts=2 sw=2 et

config/nvim/spell/en.utf-8.add
@@ -180,3 +180,5 @@ UQ
 testbenches
 elaborator
 PLL
+specialty
+USD

commit b8fb187cb069560e652a6163d92887e6dcfb1ac4

Fix apparent regression introduced by commit 388de30
Only triggers on my Ubuntu 20.04 work laptop for some reason.

This thing never worked anyway, so just delete it

config/nvim/lua/custom/plugins/tinymist.lua
@@ -1,17 +1,0 @@
-return {
-  -- add tinymist to lspconfig
-  {
-    "neovim/nvim-lspconfig",
-    opts = {
-      servers = {
-        tinymist = {
-          settings = {
-              systemFonts = false,
-              formatterMode = "typstyle",
-              previewFeature = "disable"
-          }
-        },
-      },
-    },
-  },
-}