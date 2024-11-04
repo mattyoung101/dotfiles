e0f7b7b83a0e79ffb0c959e7d02949ac157c684c
Matt Young <matt.young.1@outlook.com>
2024-11-05 00:45:38 +1000

    Add markdown-toc

---
+++ config/nvim/lua/custom/plugins/markdown-toc.lua
@@ -0,0 +1,11 @@
+return {
+    "hedyhli/markdown-toc.nvim",
+    ft = "markdown",  -- Lazy load on markdown filetype
+    cmd = { "Mtoc" }, -- Or, lazy load on "Mtoc" command
+    opts = {
+        -- Your configuration here (optional)
+    },
+    config = function()
+        require("mtoc").setup {}
+    end,
+}
