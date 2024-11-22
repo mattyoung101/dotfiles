9517a09efd222f178e38a2ede9bd0d64a395596f
Matt Young <matt.young.1@outlook.com>
2024-11-23 04:34:04 +1000

    Work around upstream Rust LSP issue

---
+++ config/nvim/after/ftplugin/rust.lua
@@ -0,0 +1,12 @@
+-- See:
+-- https://github.com/jonasmalacofilho/dotfiles/blob/main/nvim/after/ftplugin/rust.lua
+-- https://github.com/neovim/neovim/issues/30985
+for _, method in ipairs({ 'textDocument/diagnostic', 'workspace/diagnostic' }) do
+    local default_diagnostic_handler = vim.lsp.handlers[method]
+    vim.lsp.handlers[method] = function(err, result, context, config)
+        if err ~= nil and err.code == -32802 then
+            return
+        end
+        return default_diagnostic_handler(err, result, context, config)
+    end
+end
