7d9c8d2c6fb29d083f89f3216266adc066ed9bf2
Matt Young <matt.young.1@outlook.com>
2025-01-21 04:21:00 +1000

    nvim: Add telescope <leader>ta binding to search with Aerial

--- config/nvim/lua/custom/plugins/aerial.lua
+++ config/nvim/lua/custom/plugins/aerial.lua
@@ -12,5 +12,31 @@ return {
             backends = { "lsp", "treesitter", "markdown", "asciidoc", "man" },
             filter_kind = false,
         }
+
+        require("telescope").setup({
+            extensions = {
+                aerial = {
+                    -- Set the width of the first two columns (the second
+                    -- is relevant only when show_columns is set to 'both')
+                    col1_width = 4,
+                    col2_width = 30,
+                    -- How to format the symbols
+                    format_symbol = function(symbol_path, filetype)
+                        if filetype == "json" or filetype == "yaml" then
+                            return table.concat(symbol_path, ".")
+                        else
+                            return symbol_path[#symbol_path]
+                        end
+                    end,
+                    -- Available modes: symbols, lines, both
+                    show_columns = "both",
+                },
+            },
+        })
+
+        require("telescope").load_extension("aerial")
+
+
+        vim.keymap.set('n', '<leader>sa', require("telescope").extensions.aerial.aerial, { desc = '[S]earch [A]erial' })
     end,
 }
