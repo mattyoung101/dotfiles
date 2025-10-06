7ad67efdb20a6db8015a042c35fb7a2d82cc68f8
Matt Young <matt@mlyoung.cool>
2025-10-06 20:19:38 +1000

    nvim: add neogen, strudel, suppercollider

---
+++ config/nvim/lua/custom/plugins/neogen.lua
@@ -0,0 +1,13 @@
+return {
+    "danymat/neogen",
+    config = true,
+    keys = {
+        {
+            "<leader>nf",
+            "<cmd>Neogen<cr>",
+            desc = "[N]eogen Annotate",
+        },
+    }
+    -- Uncomment next line if you want to follow only stable versions
+    -- version = "*"
+}
